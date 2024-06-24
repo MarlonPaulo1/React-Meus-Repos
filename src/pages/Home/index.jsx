9afa782aa68d755fea7272b352904047c65f6ad0
Marlon Paulo <marlonpaulo1@gmail.com>
2024-06-24 15:56:10 -0300

    Criando componente de adição de novos repositórios

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,24 @@
+import { FaGithub, FaPlus } from 'react-icons/fa'
+import { Container, Form, SubmitButton } from "./styles";
+
 export default function Home() {
     return (
-        <h1>Home</h1>
+        <Container>
+            <h1>
+                <FaGithub size={25} />
+                Meus Repositorios
+            </h1>
+
+            <Form onSubmit={() => { }}>
+                <input
+                    type='text'
+                    placeholder='Adicionar Repositorios'
+                />
+
+                <SubmitButton>
+                    <FaPlus color='#fff' size={14} />
+                </SubmitButton>
+            </Form>
+        </Container>
     )
 }
