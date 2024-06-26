221daf1921b3a0442c771225601e95a59e6a9521
Marlon Paulo <marlonpaulo1@gmail.com>
2024-06-25 21:21:50 -0300

    Renderizando lista de repositorio

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
-import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa'
-import { Container, Form, SubmitButton } from "./styles";
+import { FaGithub, FaPlus, FaSpinner, FaBars } from 'react-icons/fa'
+import { GoStar, GoRepoForked, GoX } from 'react-icons/go'
+import { Container, Form, SubmitButton, List, Search, CardHeader, User, CardContent, CardFooter, DeleteButton } from "./styles";
 import { useCallback, useState } from 'react';
 import api from '../../services/api'
 
@@ -19,10 +20,18 @@ export default function Home() {
 
                 const data = {
                     name: response.data.full_name,
+                    description: response.data.description,
+                    created_at: response.data.created_at,
+                    forks: response.data.forks,
+                    stars: response.data.stargazers_count,
+                    avatar: response.data.organization.avatar_url,
+                    login: response.data.organization.login
                 }
 
                 setRepositorios([...repositorios, data])
                 setNewRepo('')
+
+                console.log(response)
             } catch (error) {
                 console.log(error)
             } finally {
@@ -39,27 +48,57 @@ export default function Home() {
 
     return (
         <Container>
-            <h1>
-                <FaGithub size={25} />
-                Meus Repositorios
-            </h1>
+            <Search>
+                <h1>
+                    <FaGithub size={25} />
+                    Meus Repositorios
+                </h1>
 
-            <Form onSubmit={handleSubmit}>
-                <input
-                    type='text'
-                    placeholder='Adicionar Repositorios'
-                    value={newRepo}
-                    onChange={handleinputChange}
-                />
+                <Form onSubmit={handleSubmit}>
+                    <input
+                        type='text'
+                        placeholder='Adicionar Repositorios'
+                        value={newRepo}
+                        onChange={handleinputChange}
+                    />
 
-                <SubmitButton loading={loading ? 1 : 0}>
-                    {loading ? (
-                        <FaSpinner color='fff' size={14} />
-                    ) : (
-                        <FaPlus color='#fff' size={14} />
-                    )}
-                </SubmitButton>
-            </Form>
+                    <SubmitButton loading={loading ? 1 : 0}>
+                        {loading ? (
+                            <FaSpinner color='fff' size={14} />
+                        ) : (
+                            <FaPlus color='#fff' size={14} />
+                        )}
+                    </SubmitButton>
+                </Form>
+            </Search>
+
+
+            <List>
+                {repositorios.map(repo => (
+                    <li key={repo.name}>
+                        <CardHeader>
+                            <User>
+                                <img src={repo.avatar} alt={repo.login} />
+                                <h3>{repo.login}</h3>
+                            </User>
+                            <span>{repo.name}</span>
+                            <div></div>
+                        </CardHeader>
+                        <CardContent>{repo.description}</CardContent>
+                        <CardFooter>
+                            <p><GoRepoForked size={20} /> {repo.forks}</p>
+                            <p><GoStar size={20} /> {repo.stars}</p>
+                            <p>{repo.created_at}</p>
+                        </CardFooter>
+                        <a href="">
+                            <FaBars size={20} />
+                        </a>
+                        <DeleteButton onClick={() => { }}>
+                            <GoX size={20} />
+                        </DeleteButton>
+                    </li>
+                ))}
+            </List>
         </Container>
     )
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,11 +2,15 @@ import styled, { keyframes, css } from "styled-components";
 
 export const Container = styled.div`
     max-width: 700px;
+    margin: 80px auto;
+`
+export const Search = styled.div`
+    
     background: #fff;
     border-radius: 4px;
     box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
     padding: 30px;
-    margin: 80px auto;
+    
 
     h1 {
         font-size: 20px;
@@ -14,6 +18,7 @@ export const Container = styled.div`
         align-items: center;
         gap: 10px;
     }
+
 `
 
 export const Form = styled.form`
@@ -66,3 +71,86 @@ export const SubmitButton = styled.button.attrs(props => ({
     }
 `
 
+export const List = styled.ul`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 1rem;
+    margin-top: 50px;
+    list-style: none;
+    
+
+    li {
+      background: #fff;
+      padding: 1rem;
+      border-radius: 4px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      position: relative;
+
+      a {
+        position: absolute;
+        right: 1rem;
+        color: #0d2636;
+      }
+    }
+`
+
+export const CardHeader = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid #ddd;
+    padding-bottom: 5px;
+    span {
+        text-align: center;
+        font-size: 2rem;
+    }
+`
+
+export const User = styled.div`
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    img {
+        width: 80px;
+        border: 2px solid #0d2636;
+        border-radius: 50%;
+    }
+
+    h3 {
+        margin-top: 5px;
+    }
+
+`
+
+export const CardContent = styled.div`
+    padding: 5px 0;
+`
+
+export const CardFooter = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-top: 1px solid #ddd;
+    padding-top: 5px;
+
+    p {
+        display: flex;
+        align-items: center;
+        gap: 8px;
+    }
+`
+
+export const DeleteButton = styled.button.attrs({
+    type: "button"
+})`
+    position: absolute;
+    left: 5px;
+    top: 5px;
+    background: transparent;
+    border: none;
+    color: #0d2636;
+`
